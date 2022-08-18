8cf5ca045694eabe30c1155247960ccb58a80040
Filipe <Filipe@condado.tm>
2022-08-18 17:31:36 -0300

    laravel

---
+++ projetos-laravel/cardapio-virtual-laravel/app/Http/Controllers/olacontroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class olacontroller extends Controller
+{
+    public function helloword(){
+      return view('filipe');
+    }
+}
